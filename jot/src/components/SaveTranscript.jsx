394814bb900f50eda07b375e616de546624a5bc5
samisalehsaeed <samisalehsaeed05@gmail.com>
2025-07-09 19:05:57 +0100

    Users can change the name of their docx transcripts

--- jot/src/components/SaveTranscript.jsx
+++ jot/src/components/SaveTranscript.jsx
@@ -3,13 +3,16 @@ import { Document, Packer, Paragraph, TextRun } from "docx";
 import { useContext } from "react";
 import { AudioTranscriptContext } from "./AudioRecorder";
 import { UploadTranscriptContext } from "./UploadAudio";
+import { useState } from "react";
 
 // Documents contain sections, you can have multiple sections per document, go here to learn more about sections
 // This simple example will only contain one section
 export default function SaveTranscript() {
   const audioDocTranscript = useContext(AudioTranscriptContext);
   const uploadDocTranscript = useContext(UploadTranscriptContext);
-  const createDoc = async () => {
+  const [transcriptFileName, setTranscriptFileName] = useState();
+  const createDoc = async (e) => {
+    e.preventDefault();
     const doc = new Document({
       sections: [
         {
@@ -27,10 +30,20 @@ export default function SaveTranscript() {
       ],
     });
     const blob = await Packer.toBlob(doc);
-    saveAs(blob, "MyTranscript.docx");
+    saveAs(blob, transcriptFileName);
   };
 
-  //   FileSaver.saveAs(doc, "MyTranscription.docx");
-
-  return <button onClick={createDoc}>Create Document</button>;
+  return (
+    <>
+      <form onSubmit={createDoc}>
+        <input
+          placeholder="Transcript File Name"
+          value={transcriptFileName || ""}
+          onChange={(e) => setTranscriptFileName(e.target.value)}
+          required
+        />
+        <button type="submit">+</button>
+      </form>
+    </>
+  );
 }
